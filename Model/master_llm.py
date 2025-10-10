f21142c07675b284a758a073355126cf43272ef2
Zeynep Gülten <168850598+zeynepgltn@users.noreply.github.com>
2025-10-10 14:57:14 +0300

    a
    a

---
+++ Model/master_llm.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+
+def get_rotary_position_encoding(input: torch.Tensor, base: 10000, device="cpu"):
+    context_length, dimension = input.shape
+    assert dimension % 2 == 0, "Dimension must be even"
+
+    half_dimension = dimension // 2
+    freqs_indices = torch.arange(0, half_dimension, device=device, dtype=torch.float32)
+    freqs = 1.0 / (base ** (freqs_indices / dimension))
+    position = torch.arange(0, context_length, device=device, dtype=torch.float32)
+
+    #broadcast uyumlu hale getirme
+    angles = position[:, None] * freqs[None, :]
+    sin_angles = torch.sin(angles)
+    cos_angles = torch.cos(angles)
+
+    input_even = input[:, :half_dimension]
+    input_odd = input[:, half_dimension:]
+
+    input_rotated_even = input_even * cos_angles - input_odd * sin_angles
+    input_rotated_odd  = input_odd  * cos_angles + input_even * sin_angles
+
+    input_rotated = torch.empty_like(input)
+    input_rotated[:, :half_dimension] = input_rotated_even
+    input_rotated[:, half_dimension:] = input_rotated_odd
+
+    return input_rotated
+
+class MasterLLM(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, context_length):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        
+        self.pos_embedding = nn.Embedding(context_length, embedding_dim)
+
+        self.get_pos = get_rotary_position_encoding
+    
+
+    def forward(self, x):
+        x = self.embedding(x)#sözlük anlamları
+        x = self.get_pos(x, base=10000, device=x.device) #position anlamları
+        return x
+    
